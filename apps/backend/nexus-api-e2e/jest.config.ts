d22e1bcd261a460ea52dc12d550e6ea2631b8aca
Bambang Gunawan <bambanggunawan887@gmail.com>
2023-06-11 17:55:10 +0700

    update config nexus-api-e2e

--- apps/backend/nexus-api-e2e/jest.config.ts
+++ apps/backend/nexus-api-e2e/jest.config.ts
@@ -1,13 +1,16 @@
 /* eslint-disable */
 export default {
-  displayName: 'nexus-api-e2e',
-  preset: '../../../jest.preset.js',
-  globalSetup: '<rootDir>/src/support/global-setup.ts',
-  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
-  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html']
+    displayName: 'nexus-api-e2e',
+    preset: '../../../jest.preset.js',
+    globalSetup: '<rootDir>/src/support/global-setup.ts',
+    setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+    globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]s$': [
+            'ts-jest',
+            { tsconfig: '<rootDir>/tsconfig.spec.json' },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
 };
