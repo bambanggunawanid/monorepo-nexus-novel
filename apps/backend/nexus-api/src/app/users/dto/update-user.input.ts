3761b0aea1b3e91179d715a0a565e38ea7da38ec
Bambang Gunawan <bambanggunawan887@gmail.com>
2023-06-11 17:53:12 +0700

    add user module

---
+++ apps/backend/nexus-api/src/app/users/dto/update-user.input.ts
@@ -0,0 +1,6 @@
+export class UpdateUserDto {
+    email: string;
+    username: string;
+    fullname: string;
+    isActive: boolean;
+}
---
+++ apps/backend/nexus-api/src/app/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, Generated, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({ unique: true, nullable: false })
+    @Generated('uuid')
+    uuid: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    fullname: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+}
