12b0b1533e723d742210c3d9c067660e508d686a
Bambang Gunawan <bambanggunawan887@gmail.com>
2023-07-08 19:49:52 +0700

    add new app in core team

--- apps/core/nexus-cloud-web/src/app/app.spec.tsx
+++ apps/core/nexus-cloud-web/src/app/app.spec.tsx
@@ -20,6 +20,6 @@ describe('App', () => {
                 <App />
             </BrowserRouter>
         );
-        expect(getByText(/Welcome core-nexus-cloud-web/gi)).toBeTruthy();
+        expect(getByText(/Welcome payment-payment-web/gi)).toBeTruthy();
     });
 });
--- apps/core/nexus-cloud-web/src/main.tsx
+++ apps/core/nexus-cloud-web/src/main.tsx
@@ -1,16 +1,14 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './app/app';
+import './app/index.css';
+import './app/satoshi.css';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
-    <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
