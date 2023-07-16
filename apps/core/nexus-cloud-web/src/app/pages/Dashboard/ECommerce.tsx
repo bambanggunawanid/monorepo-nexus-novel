bd1eae6d3fa3cbdb20eb2712ffa34333375c015b
Bambang Gunawan <bambanggunawan887@gmail.com>
2023-07-16 17:13:00 +0700

    fix tsx cannot read

--- apps/core/nexus-cloud-web/src/app/pages/Dashboard/ECommerce.tsx
+++ apps/core/nexus-cloud-web/src/app/pages/Dashboard/ECommerce.tsx
@@ -1,13 +1,13 @@
-import CardFour from '../../components/CardFour.tsx';
-import CardOne from '../../components/CardOne.tsx';
-import CardThree from '../../components/CardThree.tsx';
-import CardTwo from '../../components/CardTwo.tsx';
-import ChartOne from '../../components/ChartOne.tsx';
-import ChartThree from '../../components/ChartThree.tsx';
-import ChartTwo from '../../components/ChartTwo.tsx';
-import ChatCard from '../../components/ChatCard.tsx';
-import MapOne from '../../components/MapOne.tsx';
-import TableOne from '../../components/TableOne.tsx';
+import CardFour from '../../components/CardFour';
+import CardOne from '../../components/CardOne';
+import CardThree from '../../components/CardThree';
+import CardTwo from '../../components/CardTwo';
+import ChartOne from '../../components/ChartOne';
+import ChartThree from '../../components/ChartThree';
+import ChartTwo from '../../components/ChartTwo';
+import ChatCard from '../../components/ChatCard';
+import MapOne from '../../components/MapOne';
+import TableOne from '../../components/TableOne';
 
 const ECommerce = () => {
     return (
